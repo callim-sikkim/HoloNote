5430dd038ee55a3259ee7cd8adcd0bac3d11d490
Kamil Cieślik <cieslik.kamil@outlook.com>
2024-08-26 13:08:05 +0200

    Add working Db connection - check by apply notes migration

---
+++ HoloNote.DbConnection/HoloNoteDbContext.cs
@@ -0,0 +1,12 @@
+﻿using HoloNote.Dto;
+using Microsoft.EntityFrameworkCore;
+
+namespace HoloNote.DbConnection;
+
+public class HoloNoteDbContext : DbContext
+{
+    public HoloNoteDbContext(DbContextOptions<HoloNoteDbContext> options) : base(options)
+    {
+    }
+    DbSet<Note> Notes { get; set; }
+}
---
+++ HoloNote.DbConnection/HoloNoteDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace HoloNote.DbConnection;
+
+public class HoloNoteDbContextFactory : IDesignTimeDbContextFactory<HoloNoteDbContext>
+{
+    public HoloNoteDbContext CreateDbContext(string[] args)
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var builder = new DbContextOptionsBuilder<HoloNoteDbContext>();
+        var connectionString = configuration.GetConnectionString("LocalDb");
+
+        builder.UseSqlServer(connectionString);
+        return new HoloNoteDbContext(builder.Options);
+    }
+}
